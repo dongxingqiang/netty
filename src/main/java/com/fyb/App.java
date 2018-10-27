69930fbcba588aabbf70fcfc6c1392b6c8d8adba
daben <1690041432@qq.com>
2018-10-27 12:20:16 +0800

    第一次提交

---
+++ src/main/java/com/fyb/App.java
@@ -0,0 +1,13 @@
+package com.fyb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
